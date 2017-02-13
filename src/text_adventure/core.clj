1e4a8f96794fa9dc9eb2cdfe4c28ec6f693f4b18
Craig Eales <craig.eales@gmail.com>
2017-02-13 20:42:58 +0000

    Add inventory and game procs

--- src/text_adventure/core.clj
+++ src/text_adventure/core.clj
@@ -1,19 +1,27 @@
 (ns text-adventure.core
   (:gen-class))
 
+(defn dungeon-proc [state]
+  (if (> (rand 1) 0.9)
+    {:message "You were eaten by a grue"
+     :new-state (assoc-in state [:player :alive] false)}
+    {:new-state state}))
+
 (def rooms
   {:hallway {:east :sauna
              :north :bedroom
              :description "Here be dragons.. in a painting on the wall"}
    :sauna {:west :hallway
-           :description "This is the sauna, with some chains in the corner, and a fat man lying sleeping in the corner"
-           :objects #{:dungeon-key}}
+           :description "This is the sauna, with some chains in the corner, and a fat man lying sleeping in the corner"}
    :bedroom {:south :hallway
              :north :dungeon
              :description "The room is empty"}
    :dungeon {:south :hallway
              :locked true
-             :description "A dark dimly lit brick room"}})
+             :description "It is pitch black. You are likely to be eaten by a grue"
+             :proc dungeon-proc}})
+
+(def items {:sauna #{:dungeon-key}})
 
 (defn can-enter? [room-key inventory]
   (cond
@@ -26,26 +34,31 @@
     (can-enter? new-room inventory)))
 
 (defn move-room [state direction]
-  (if-let [new-room (move-direction (:room state) direction (:inventory state))]
+  (if-let [new-room (move-direction (-> state :player :room) direction (-> state :player :inventory))]
     (if (= new-room :locked)
       ["The room was locked and you don't have the key" state]
-      ["You moved rooms" (update state :room (constantly new-room))])
-    ["There wasn't a room in that direction" state]))
+      ["You moved rooms" (assoc-in state [:player :room] new-room)])
+    ["There wasn't a room in that direction"
+     state]))
 
-(defn pickup-object [{:keys [room] :as state} object-name]
-  (if-let [object (get (:objects (rooms room)) object-name)]
-    [(str "Picked up " object) (update state :inventory conj object)]
-    ["couldn't find anything" state]))
+(defn pickup-object [{:keys [player world] :as state} object-name]
+  (if-let [object (get ((:items world) (:room player)) object-name)]
+    [(str "Picked up " object)
+     (-> state
+         (update-in [:player :inventory] conj object)
+         (update-in [:world :items (:room player)] disj object))]
+    ["couldn't find anything"
+     state]))
 
-(defn command-person [command {:keys [room] :as state}]
+(defn command-person [command state]
   (case command
     "move east" (move-room state :east)
     "move west" (move-room state :west)
     "move north" (move-room state :north)
     "move south" (move-room state :south)
     "pickup dungeon key" (pickup-object state :dungeon-key)
-    "suicide" ["you are dead" (assoc state :alive false)]
-    "exit" ["goodbye" (assoc state :alive false)]
+    "suicide" ["you are dead" (assoc-in state [:player :alive] false)]
+    "exit" ["goodbye" (assoc-in state [:player  :alive] false)]
     ["I couldn't understanted me to do" state])
 
   ;return new state
@@ -53,19 +66,28 @@
 
 ;; ------------------
 
-(defn describe [state]
-  (println (str "You're currently in the " (name (:room state)) " - " (->> state :room (get rooms) :description)))
-  (if-let [objects (seq (:objects (rooms (:room state))))]
+(defn describe [{:keys [player world]}]
+  (println (str "You're currently in the " (name (:room player)) " - " (->> player :room (get rooms) :description)))
+  (if-let [objects (seq ((:items world) (:room player)))]
     (println "You can see: " objects)))
 
 (defn -main []
-  (loop [state {:room :hallway
-                :alive true
-                :inventory #{}}]
-    (when (:alive state)
+  (loop [state {:player  {:room :hallway
+                          :alive true
+                          :inventory #{}
+                          }
+                :world   {:items items}}]
+    (when (-> state :player :alive)
       (describe state)
       (println "What's your next command")
       (let [command (read-line)
             [message new-state] (command-person command state)]
         (println message)
-        (recur new-state)))))
+        (recur
+         (if-let [proc (:proc (rooms (-> new-state :player :room)))]
+           (let [{:keys [message new-state]} (proc new-state)]
+             (when message
+               (println message))
+             new-state)
+           new-state))
+        ))))
